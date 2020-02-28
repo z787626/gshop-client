8eb019537a45b0b321e226dfff2f4648dde4d5b8
zpx <787626485@qq.com>
2020-02-28 15:32:45 +0800

    2

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import MSite from '../pages/Msite/Msite'
 import Search from '../pages/Search/Search'
 import Order from '../pages/Order/Order'
 import Profile from '../pages/Profile/Profile'
+import Login from '../pages/Login/Login'
 // 声明使用插件
 Vue.use(VueRouter)
 
@@ -15,19 +16,35 @@ export default new VueRouter({
   routes: [
     {
       path: '/msite',
-      component: MSite
+      component: MSite,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/search',
-      component: Search
+      component: Search,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/order',
-      component: Order
+      component: Order,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/profile',
-      component: Profile
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
     },
     {
       path: '/',
